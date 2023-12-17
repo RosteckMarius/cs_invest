0a7f3ed26867ba4cfd810509507ce156b0373678
marius <marius.rosteck@gmail.com>
2023-12-17 08:05:22 +0100

    Add Footer

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,11 @@
+export function Footer() {
+    return (
+        <div className={"fixed bottom-0 left-0 w-full border-t-2 py-4"}>
+            <div className={"flex justify-center gap-4"}>
+                <span>About Us</span>
+                <span>FAQs</span>
+                <span>Contact</span>
+            </div>
+        </div>
+    );
+}
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet, useLocation } from "react-router-dom";
 import { FloatingBackButton } from "./FloatingBackButton.tsx";
+import { Footer } from "./Footer.tsx";
 
 export function Layout() {
     const location = useLocation();
     const backButtonVisible = location.pathname !== "/";
     return (
-        <>
+        <div>
             {backButtonVisible && <FloatingBackButton />}
             <Outlet />
-        </>
+            <Footer></Footer>
+        </div>
     );
 }
